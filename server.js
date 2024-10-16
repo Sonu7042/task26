14af7f5acc13665b9a278fecb099aa02736198c1
SonuSingh05 <sonusinghmourya143@gmail.com>
2024-10-16 20:38:33 +0530

    task26

---
+++ server.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(path.join(__dirname, "./Pages/home.html"), (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("500 internal server error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+
+  } else if (req.url === "/about") {
+    fs.readFile(path.join(__dirname, "./Pages/about.html"), (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("500 internal server error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+
+  } else if (req.url === "/contact") {
+    fs.readFile(path.join(__dirname, "./Pages/contact.html"), (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("500 internal server error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 - Not Found");
+  }
+});
+
+
+
+
+server.listen(9000, () => {
+  console.log("server is listening..");
+});
